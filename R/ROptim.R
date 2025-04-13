0a48df8610e0b6b8e2de759675e5208d13684b9f
PrzeChoj <premysl.choj@gmail.com>
2025-04-13 23:14:44 +0200

    styler

--- R/ROptim.R
+++ R/ROptim.R
@@ -3,7 +3,7 @@
 
 #' Fast optimization for the corelation matrix
 #'
-#' Part of the PCGLASSO function.
+#' Part of the [pcglassoFast()] function.
 #' This implementation is a small modification of the `glassoFast` function from `glassoFast` package.
 #'
 #' @param S Covariance matrix (a p by p symmetric matrix).
@@ -24,60 +24,62 @@
 #' }
 #'
 #' @keywords glasso covariance matrix regularization penalized likelihood
-ROptim <- function(S, rho, thr=1.0e-4, maxIt=1e4, start=c("cold","warm"), w.init=NULL, wi.init=NULL, trace=FALSE){
-  n=nrow(S)           # dimension of S
-  if(is.matrix(rho)){
-      if(length(rho)!=n*n) stop("The input matrix for \"rho\" must be of size ",n," by ",n)
-      L = rho         # matrix of regularization parameters
-  }else{
-      L = matrix(rho,n,n) # matrix of regularization parameters
+ROptim <- function(S, rho, thr = 1.0e-4, maxIt = 1e4, start = c("cold", "warm"), w.init = NULL, wi.init = NULL, trace = FALSE) {
+  n <- nrow(S) # dimension of S
+  if (is.matrix(rho)) {
+    if (length(rho) != n * n) stop("The input matrix for \"rho\" must be of size ", n, " by ", n)
+    L <- rho # matrix of regularization parameters
+  } else {
+    L <- matrix(rho, n, n) # matrix of regularization parameters
   }
 
   # cold or warm start
-  start.type=match.arg(start)
-  if(start.type=="cold"){
-    is=0
-    W=X=matrix(0,nrow=n,ncol=n)
+  start.type <- match.arg(start)
+  if (start.type == "cold") {
+    is <- 0
+    W <- X <- matrix(0, nrow = n, ncol = n)
   }
-  if(start.type=="warm"){
-    is=1
-    if(is.null(w.init) | is.null(wi.init)){
+  if (start.type == "warm") {
+    is <- 1
+    if (is.null(w.init) | is.null(wi.init)) {
       stop("Warm start specified: w.init and wi.init must be non-null")
     }
-    W=w.init
-    X=wi.init
+    W <- w.init
+    X <- wi.init
   }
 
-  Wd = WXj = numeric(n)
+  Wd <- WXj <- numeric(n)
 
-  msg=1*trace
-  info = 0
-  mode(n)="integer"
-  mode(S)="double"
-  mode(L)="double"
-  mode(thr)="double"
-  mode(maxIt)="integer"
-  mode(msg)="integer"
-  mode(is)="integer"
-  mode(X)="double"
-  mode(W)="double"
-  mode(info)="integer"
+  msg <- 1 * trace
+  info <- 0
+  mode(n) <- "integer"
+  mode(S) <- "double"
+  mode(L) <- "double"
+  mode(thr) <- "double"
+  mode(maxIt) <- "integer"
+  mode(msg) <- "integer"
+  mode(is) <- "integer"
+  mode(X) <- "double"
+  mode(W) <- "double"
+  mode(info) <- "integer"
 
 
-  LASSO<-.Fortran("roptim",
-                 n,
-                 S,
-                 L,
-                 thr,
-                 maxIt,
-                 msg,
-                 is,
-                 X,
-                 W,
-                 Wd,
-                 WXj,
-                 info)
+  LASSO <- .Fortran(
+    "roptim",
+    n,
+    S,
+    L,
+    thr,
+    maxIt,
+    msg,
+    is,
+    X,
+    W,
+    Wd,
+    WXj,
+    info
+  )
 
-  results <- list(w=LASSO[[9]], wi=LASSO[[8]], errflag=LASSO[[12]], niter=LASSO[[5]])
+  results <- list(w = LASSO[[9]], wi = LASSO[[8]], errflag = LASSO[[12]], niter = LASSO[[5]])
   return(results)
 }
